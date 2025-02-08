c7a8499b206093899b9dbae04202ca98c037eb30
Hung <hung01590159@gmail.com>
2025-02-08 16:08:46 +0700

    upload code 2 links routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import './App.css';
-import Header from './components/Header/Header';
+import "./App.css";
+import Header from "./components/Header/Header";
+import { Outlet, Link } from "react-router-dom";
 
 const App = () => {
-
   return (
     <div className="app-container">
-      <Header />
+      <div className="header-container">
+        <Header />
+      </div>
+      <div className="main-container">
+        <div className="sidenav-container">
+          sidenav
+        </div>
+        <div className="app-content">
+            <Outlet />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,25 +2,24 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className="bg-body-tertiary mb-4">
       <Container>
         <Navbar.Brand href="#home">CyberSoft</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Users</Nav.Link>
-            <Nav.Link href="#link">Admin</Nav.Link>
+            <Link className="nav-link" to="/">Home</Link>
+            <Link className="nav-link" to="/users">Users</Link>
+            <Link className="nav-link" to="/admins">Admins</Link>
           </Nav>
           <Nav>
             <NavDropdown title="Settings" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Log in</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-              Log out
-              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Log out</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Profile</NavDropdown.Item>
             </NavDropdown>
           </Nav>
