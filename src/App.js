fc19ac1f60628c4415abdcca75d11d890547fb42
Hung <hung01590159@gmail.com>
2025-02-08 12:08:38 +0700

    init project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+
+const App = () => {
+
+  return (
+    <div className="app-container">
+      Hello world!123
+    </div>
+  );
+}
+
+export default App;
