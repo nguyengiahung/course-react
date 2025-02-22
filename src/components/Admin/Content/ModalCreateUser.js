b7364cac7d1b87f9c1fc86aa99e68f749ceb2fe0
Hung <hung01590159@gmail.com>
2025-02-22 10:15:45 +0700

    upload code react toastify

--- src/components/Admin/Content/ModalCreateUser.js
+++ src/components/Admin/Content/ModalCreateUser.js
@@ -2,19 +2,20 @@ import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import { FcPlus } from "react-icons/fc";
-import axios from 'axios';
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const ModalCreateUser = () => {
   const [show, setShow] = useState(false);
 
   const handleClose = () => {
-    setShow(false)
-    setEmail("")
-    setPassword("")
-    setUsername("")
-    setRole("")
-    setImage("")
-    setPreviewImage("")
+    setShow(false);
+    setEmail("");
+    setPassword("");
+    setUsername("");
+    setRole("");
+    setImage("");
+    setPreviewImage("");
   };
   const handleShow = () => setShow(true);
 
@@ -32,11 +33,30 @@ const ModalCreateUser = () => {
     }
   };
 
+  const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
+
   const handleSubmitCreateUser = async () => {
     //validate
+    const isValidEmail = validateEmail(email);
+    if (!isValidEmail) {
+      toast.error("Invalid Email!");
+      return;
+    }
+    
+    if (!password) {
+      toast.error("Invalid Password!");
+      return;
+    }
+
+
 
     // call apis
-
     const data = new FormData();
     data.append("email", email);
     data.append("password", password);
@@ -44,8 +64,21 @@ const ModalCreateUser = () => {
     data.append("role", role);
     data.append("userImage", image);
 
-    let res = await axios.post("http://localhost:8081/api/v1/participant", data);
-    console.log('check res', res);
+    let res = await axios.post(
+      "http://localhost:8081/api/v1/participant",
+      data
+    );
+    console.log("check res", res.data);
+
+    if (res.data && res.data.EC === 0) {
+      toast.success(res.data.EM);
+      handleClose();
+    }
+
+    if (res.data && res.data.EC !== 0) {
+      toast.error(res.data.EM);
+    }
+
   };
 
   return (
