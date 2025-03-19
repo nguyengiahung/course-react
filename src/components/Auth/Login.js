6b213962b3881970def1b455440970f72fcb5369
Hung <hung01590159@gmail.com>
2025-03-19 09:10:13 +0700

    upload code redux user

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -3,10 +3,12 @@ import "./Login.scss";
 import { useState } from "react";
 import { postLogin } from "../../services/apiService";
 import { toast } from "react-toastify";
+import { useDispatch } from "react-redux";
+import { doLogin } from "../../redux/action/userAction";
 
 const Login = (props) => {
   const navigate = useNavigate();
-
+  const dispatch = useDispatch();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
@@ -33,9 +35,9 @@ const Login = (props) => {
 
     // submit api
     let data = await postLogin(email, password);
-    console.log(data);
 
     if (data && data.EC === 0) {
+      dispatch(doLogin(data))
       toast.success(data.EM);
       navigate("/");
     }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,11 +2,14 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import { useSelector } from "react-redux";
 import { Link, NavLink, useNavigate } from "react-router-dom";
 
 const Header = () => {
   const navigate = useNavigate();
-
+  const account = useSelector((state) => state.user.account);
+  const isAuthenticated = useSelector((state) => state.user.isAuthenticated);
+  console.log(account, isAuthenticated);
   return (
     <Navbar expand="lg" className="bg-body-tertiary mb-5">
       <Container>
@@ -27,13 +30,27 @@ const Header = () => {
             </NavLink>
           </Nav>
           <Nav>
-            <button className="btn-login" onClick={() => navigate('/login')}>Log in</button>
-            <button className="btn-dark mx-4" onClick={() => navigate('/register')}>Sign up</button>
-            {/* <NavDropdown title="Settings" id="basic-nav-dropdown">
-              <NavDropdown.Item>Log in</NavDropdown.Item>
-              <NavDropdown.Item>Log out</NavDropdown.Item>
-              <NavDropdown.Item>Profile</NavDropdown.Item>
-            </NavDropdown> */}
+            {isAuthenticated ? (
+              <NavDropdown title="Settings" id="basic-nav-dropdown">
+                <NavDropdown.Item>{account.username}</NavDropdown.Item>
+                <NavDropdown.Item>Log out</NavDropdown.Item>
+              </NavDropdown>
+            ) : (
+              <>
+                <button
+                  className="btn-login"
+                  onClick={() => navigate("/login")}
+                >
+                  Log in
+                </button>
+                <button
+                  className="btn-dark mx-4"
+                  onClick={() => navigate("/register")}
+                >
+                  Sign up
+                </button>
+              </>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>
