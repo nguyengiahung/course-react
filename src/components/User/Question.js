ff64861b69a13ce46905cbcb43d70eeb0e286c4f
Hung <hung01590159@gmail.com>
2025-04-19 09:46:56 +0700

    upload code css question right

--- src/components/User/Question.js
+++ src/components/User/Question.js
@@ -1,7 +1,11 @@
 import _ from "lodash";
+import { useState } from "react";
+import Lightbox from "react-awesome-lightbox";
 
 const Question = (props) => {
   const { data, index, handleCheckBox } = props;
+  const [isPreviewImage, setIsPreviewImage] = useState(false);
+
   if (_.isEmpty(data)) {
     return <></>;
   }
@@ -16,7 +20,18 @@ const Question = (props) => {
     <>
       {data.image ? (
         <div className="q-image text-center">
-          <img src={`data:image/jpeg;base64,${data.image}`} />
+          <img
+          style={{ cursor: 'pointer' }}
+            src={`data:image/jpeg;base64,${data.image}`}
+            onClick={() => setIsPreviewImage(true)}
+          />
+          {isPreviewImage && (
+            <Lightbox
+              image={`data:image/jpeg;base64,${data.image}`}
+              title={"Question Image"}
+              onClose={() => setIsPreviewImage(false)}
+            ></Lightbox>
+          )}
         </div>
       ) : (
         <div className="q-image"></div>
